d707dd3874df5038394c577a8231d97bbeddd384
akashyadav2310 <akashyadav231099@gmail.com>
2024-05-03 12:21:28 +0530

    Restructure the entities names for Booking and Event model

--- eventspot-booking-service/src/main/java/com/eventspot/bookingservice/model/Booking.java
+++ eventspot-booking-service/src/main/java/com/eventspot/bookingservice/model/Booking.java
@@ -6,6 +6,7 @@ import java.util.Date;
 @Entity
 @Table(name = "bookings")
 public class Booking {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,5 +28,76 @@ public class Booking {
 
     @Column(name = "total_price", nullable = false)
     private double totalPrice;
+
+    public Booking() {
+      
+    }
+    
+    public Booking(Long id, Long eventId, Long userId, Date bookingDate, String bookingStatus,
+        int ticketCount, double totalPrice) {
+        this.id = id;
+        this.eventId = eventId;
+        this.userId = userId;
+        this.bookingDate = bookingDate;
+        this.bookingStatus = bookingStatus;
+        this.ticketCount = ticketCount;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getId() {
+      return id;
+    }
+
+    public void setId(Long id) {
+      this.id = id;
+    }
+
+    public Long getEventId() {
+      return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+      this.eventId = eventId;
+    }
+
+    public Long getUserId() {
+      return userId;
+    }
+
+    public void setUserId(Long userId) {
+      this.userId = userId;
+    }
+
+    public Date getBookingDate() {
+      return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+      this.bookingDate = bookingDate;
+    }
+
+    public String getBookingStatus() {
+      return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+      this.bookingStatus = bookingStatus;
+    }
+
+    public int getTicketCount() {
+      return ticketCount;
+    }
+
+    public void setTicketCount(int ticketCount) {
+      this.ticketCount = ticketCount;
+    }
+
+    public double getTotalPrice() {
+      return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+      this.totalPrice = totalPrice;
+    }
     
 }
